e4fca36458ab762e5bd60ecb90d62f8c0136ed72
Fachry Azaria <fahryazaria@gmail.com>
2023-10-05 23:36:59 +0800

    merge FAQ

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import AdminPage from "./pages/AdminPage";
 import Home from "./pages/home";
 
+
 const App = () => {
   return (
     <>
